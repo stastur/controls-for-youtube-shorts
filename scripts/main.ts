64508d568a86125716ec7bfc471dc09d1ce82fa2
Stanislau Tur <stanislau.tur@gmail.com>
2023-11-12 13:40:41 +0100

    Few UX tweaks

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,44 +1,45 @@
-(async () => {
-  const LOG_NS = "[Controlled shorts]";
-  const log = console.log.bind(console, LOG_NS);
-  const warn = console.warn.bind(console, LOG_NS);
+const LOG_NS = "[Controlled shorts]";
+const log = console.log.bind(console, LOG_NS);
+const warn = console.warn.bind(console, LOG_NS);
 
-  function waitForElement(
-    selector: string,
-    options: { context?: HTMLElement; limit?: number } = {}
-  ): Promise<HTMLElement> {
-    const context = options?.context ?? document;
-    const limit = options?.limit ?? 5000;
+function waitForElement(
+  selector: string,
+  options: { context?: HTMLElement; limit?: number } = {}
+): Promise<HTMLElement> {
+  const context = options?.context ?? document;
+  const limit = options?.limit ?? 5000;
 
-    let intervalId: number;
+  let intervalId: number;
 
-    const element = new Promise<HTMLElement>((resolve) => {
-      intervalId = setInterval(() => {
-        const element = context.querySelector<HTMLElement>(selector);
+  const elementPromise = new Promise<HTMLElement>((resolve) => {
+    intervalId = setInterval(() => {
+      const found = context.querySelector<HTMLElement>(selector);
+      if (found) {
+        clearInterval(intervalId);
+        resolve(found);
+      }
+    }, 100);
+  });
 
-        if (element) {
-          clearInterval(intervalId);
-          resolve(element);
-        }
-      }, 100);
-    });
+  return Promise.race([
+    elementPromise,
+    timeout(limit) as Promise<HTMLElement>,
+  ]).finally(() => clearInterval(intervalId));
+}
 
-    return Promise.race([element, timeout(limit) as never]).finally(() =>
-      clearInterval(intervalId)
-    );
-  }
+function timeout(time: number) {
+  return new Promise((_, reject) =>
+    setTimeout(() => reject(new Error("timeout")), time)
+  );
+}
 
-  function timeout(time: number) {
-    return new Promise((_, reject) => setTimeout(reject, time));
-  }
+function round(n: number) {
+  return Math.round(n * 100) / 100;
+}
 
-  function round(n: number) {
-    return Math.round(n * 100) / 100;
-  }
-
-  class ProgressBar extends EventTarget {
-    static id = "controlled-shorts-progress-bar";
-    static styles = `
+class ProgressBar extends EventTarget {
+  static id = "controlled-shorts-progress-bar";
+  static styles = `
 :host {
   --progress: 0;
 }
@@ -88,14 +89,16 @@
 }
   `;
 
-    private host = document.createElement("div");
-    private shadowRoot = this.host.attachShadow({ mode: "open" });
+  private host: HTMLDivElement;
+  private shadowRoot: ShadowRoot;
+  private container: HTMLElement;
 
-    constructor() {
-      super();
-
-      this.host.id = ProgressBar.id;
-      this.shadowRoot.innerHTML = `
+  constructor() {
+    super();
+    this.host = document.createElement("div");
+    this.host.id = ProgressBar.id;
+    this.shadowRoot = this.host.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
 <style>${ProgressBar.styles}</style>
 
 <div class="container">
@@ -104,94 +107,108 @@
   <div class="progress"></div>
 </div>
     `;
+    this.container = this.shadowRoot.querySelector(".container")!;
 
-      this.initEvents();
-    }
-
-    setStyle(styles: Partial<CSSStyleDeclaration>) {
-      Object.assign(this.host.style, styles);
-    }
-
-    setProgress(progress: number) {
-      this.host.style.setProperty("--progress", `${round(progress)}`);
-    }
-
-    initEvents() {
-      const container = this.shadowRoot.querySelector(
-        ".container"
-      ) as HTMLElement;
-
-      container.addEventListener("click", (e) => {
-        const { left, width } = container.getBoundingClientRect();
-        const progress = (e.clientX - left) / width;
-
-        this.setProgress(progress);
-        this.dispatchEvent(new CustomEvent("seek", { detail: progress }));
-      });
-    }
-
-    render(container: HTMLElement) {
-      container.appendChild(this.host);
-    }
+    this.initEvents();
   }
 
-  async function initProgressBar() {
-    const renderer = await waitForElement("ytd-reel-video-renderer[is-active]");
-    const video = (await waitForElement("video", {
-      context: renderer,
-    })) as HTMLVideoElement;
+  private seek(e: MouseEvent | PointerEvent) {
+    const { left, width } = this.container.getBoundingClientRect();
+    const progress = (e.clientX - left) / width;
 
-    const progressBar = new ProgressBar();
-    progressBar.setStyle({
-      width: "100%",
-      position: "absolute",
-      bottom: "0",
+    this.setProgress(progress);
+    this.dispatchEvent(new CustomEvent("seek", { detail: progress }));
+  }
+
+  private initEvents() {
+    let isPointerDown = false;
+
+    this.container.addEventListener("pointerdown", (e) => {
+      e.preventDefault();
+
+      isPointerDown = true;
+      this.dispatchEvent(new CustomEvent("seekstart"));
+      this.seek(e);
     });
 
-    function timeUpdateListener() {
-      progressBar.setProgress(video.currentTime / video.duration);
-    }
+    this.container.addEventListener("pointermove", (e) => {
+      isPointerDown && this.seek(e);
+    });
 
-    function seekListener(e: Event) {
-      video.currentTime = video.duration * (e as CustomEvent).detail;
-    }
-
-    function arrowKeysListener(e: KeyboardEvent) {
-      e.key === "ArrowRight" && (video.currentTime += 5);
-      e.key === "ArrowLeft" && (video.currentTime -= 5);
-    }
-
-    document.addEventListener("keydown", arrowKeysListener);
-    progressBar.addEventListener("seek", seekListener);
-    video.addEventListener("timeupdate", timeUpdateListener);
-
-    progressBar.render(renderer);
-
-    return function cleanUp() {
-      progressBar.removeEventListener("seek", seekListener);
-      document.getElementById(ProgressBar.id)?.remove();
-      document.removeEventListener("keydown", arrowKeysListener);
-      video.removeEventListener("timeupdate", timeUpdateListener);
-    };
+    this.container.addEventListener("pointerup", () => {
+      isPointerDown = false;
+      this.dispatchEvent(new CustomEvent("seekend"));
+    });
   }
 
-  let cleanUp: () => void;
-  async function init() {
-    try {
-      cleanUp = await initProgressBar();
-    } catch {}
+  setStyle(styles: Partial<CSSStyleDeclaration>) {
+    Object.assign(this.host.style, styles);
+  }
+
+  setProgress(progress: number) {
+    this.host.style.setProperty("--progress", `${round(progress)}`);
+  }
+
+  render(container: HTMLElement) {
+    container.appendChild(this.host);
+  }
+}
+
+async function initProgressBar() {
+  const renderer = await waitForElement("ytd-reel-video-renderer[is-active]");
+  const video = (await waitForElement("video", {
+    context: renderer,
+  })) as HTMLVideoElement;
+
+  const progressBar = new ProgressBar();
+  progressBar.setStyle({
+    width: "100%",
+    position: "absolute",
+    bottom: "0",
+  });
+
+  function timeUpdateListener() {
+    progressBar.setProgress(video.currentTime / video.duration);
+  }
+
+  function arrowKeysListener(e: KeyboardEvent) {
+    e.key === "ArrowRight" && (video.currentTime += 5);
+    e.key === "ArrowLeft" && (video.currentTime -= 5);
+  }
+
+  document.addEventListener("keydown", arrowKeysListener);
+  video.addEventListener("timeupdate", timeUpdateListener);
+  progressBar.addEventListener("seekstart", () => video.pause());
+  progressBar.addEventListener("seekend", () => video.play());
+  progressBar.addEventListener("seek", (e) => {
+    video.currentTime = video.duration * (e as CustomEvent).detail;
+  });
+
+  progressBar.render(renderer);
+
+  return function cleanUp() {
+    document.getElementById(ProgressBar.id)?.remove();
+    document.removeEventListener("keydown", arrowKeysListener);
+    video.removeEventListener("timeupdate", timeUpdateListener);
+  };
+}
+
+let cleanUp: () => void;
+async function init() {
+  try {
+    cleanUp = await initProgressBar();
+  } catch {}
+}
+
+init();
+
+document.addEventListener("yt-navigate-finish", async (e) => {
+  cleanUp?.();
+
+  const typedEvent = e as CustomEvent<{ pageType: string }>;
+  if (typedEvent.detail.pageType !== "shorts") {
+    return;
   }
 
   init();
-
-  document.addEventListener("yt-navigate-finish", async (e) => {
-    cleanUp?.();
-
-    const typedEvent = e as CustomEvent<{ pageType: string }>;
-    if (typedEvent.detail.pageType !== "shorts") {
-      return;
-    }
-
-    init();
-  });
-})();
+});
